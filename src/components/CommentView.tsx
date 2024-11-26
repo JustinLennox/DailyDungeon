686922bc702749e3e927c73f8274721f4feba0d5
Justin Lennox <justinanthonylennox@gmail.com>
2024-11-25 23:21:41 -0500

    More comment view

--- src/components/CommentView.tsx
+++ src/components/CommentView.tsx
@@ -11,65 +11,72 @@ export const CommentView = (context: Devvit.Context, comment: any): JSX.Element 
 		if (!comment || !comment.authorName) {
 			return `No comment/author name for comment: ${comment}`;
 		}
-		const user = await context.reddit.getUserById(comment.auhtorName);
+		const user = await context.reddit.getUserByUsername(comment.authorName);
 		if (!user) {
 			return "No user";
 		}
 		const url = await user.getSnoovatarUrl();
 		if (!url) {
+			console.log("No snoovatar url");
 			return "No url";
 		}
+		console.log("Got snoovatar url: ", url);
 		return url ?? null;
 		// } catch (e) {
 		// 	return `Error: ${e}`;
 		// }
 	});
 
+	const navigateToComment = async () => {
+		try {
+			const redditComment = await context.reddit.getCommentById(comment.id);
+			await context.ui.navigateTo(redditComment);
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
 	return (
-		<vstack onPress={async () => {
-			try {
-				const redditComment = await context.reddit.getCommentById(comment.id);
-				await context.ui.navigateTo(redditComment);
-			} catch (e) {
-				console.log(e);
-			}
-		}}>
+		<vstack>
 			<vstack padding="medium" cornerRadius="medium" gap="small" backgroundColor='secondary-background'>
-				<hstack>
-					<vstack grow alignment='start top'>
-						<image
-							url={"x"}
-							imageHeight={30}
-							imageWidth={30}
-						/>
-						<spacer grow />
-					</vstack>
-					<vstack alignment='start top'>
-						<hstack gap='small'>
-							<text>{comment.authorName}</text>
-							{comment.dateString && (
-								<>
-									<text color='secondary-plain-weak'>•</text>
-									<text color='secondary-plain-weak'>{comment.dateString} ago</text>
-								</>
-							)}
-						</hstack>
-						<hstack padding='small'>
-							<text color='neutral-content-strong' weight='bold'>{comment.body}</text>
-						</hstack>
-						<hstack gap='small' alignment='start middle'>
-							<icon name="upvote-fill" color="upvote-plain" />
-							<text>{comment.score}</text>
-						</hstack>
-					</vstack>
-				</hstack>
-				<hstack alignment='center middle'>
+				<hstack gap='small'>
+					<image
+						url={"x"}
+						imageHeight={30}
+						imageWidth={30}
+					/>
+					<text>{comment.authorName}</text>
+					{comment.dateString && (
+						<>
+							<text color='secondary-plain-weak'>•</text>
+							<text color='secondary-plain-weak'>{comment.dateString} ago</text>
+						</>
+					)}
 					<spacer grow />
-					<text
-						color='secondary-plain-weak'
+				</hstack>
+				<hstack padding='small' alignment='center middle'>
+					<text color='neutral-content-strong'
+						weight='bold'
+						size={'medium'}
+						alignment='center middle'
+						overflow='ellipsis'
 					>
-						Top Comment
+						{comment.body}
 					</text>
+				</hstack>
+				<hstack gap='small' alignment='start middle'>
+					<icon name="upvote-fill" color="upvote-plain" />
+					<text>{comment.score}</text>
+					<spacer grow />
+					<hstack
+						onPress={async () => { await navigateToComment() }}
+						gap='small'
+					>
+						<text color='secondary-plain-weak'>
+							Top Comment
+						</text>
+						<icon name='comments' color='secondary-plain-weak' size='small' />
+					</hstack>
 				</hstack>
 			</vstack>
 		</vstack>
