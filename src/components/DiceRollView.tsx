b21c0d8bde1fc1a526c4360c39cec3aa1376beba
Justin Lennox <justinanthonylennox@gmail.com>
2024-11-25 21:00:09 -0500

    dice roll stuff

--- src/components/DiceRollView.tsx
+++ src/components/DiceRollView.tsx
@@ -1,107 +1,126 @@
 import { Devvit, useAsync, useState, Comment } from '@devvit/public-api';
+import { ExpireTime, StringDictionary } from '../utils/utils.js';
 
 export const DiceRollView = (context: Devvit.Context, gameData: any, setShowDiceView: any): JSX.Element => {
+	const totalKey = `rollTotals:${gameData.postID}`;
 
-	/// The user's roll, if they've rolled
-	const [userRoll, setUserRoll] = useState<number | null>(null);
+	const {
+		data: userRoll,
+		loading: loadingUserRoll,
+		error: userRollError,
+	} = useAsync(
+		async () => {
+			if (!gameData || !gameData.postID) {
+				throw new Error("no-post-data");
+			};
+			if (gameData.finished) {
+				throw new Error("post-finished");
+			};
+
+			try {
+				// Get user ID
+				const user = await context.reddit.getCurrentUser();
+				const userID = user?.id ?? "anonymous";
+				const postUserRollsKey = `userRolls:${gameData.postID}`;
+				context.ui.showToast(`Game id ${gameData.postID}, userID ${userID}`);
+				if (user) {
+					// Check if the user has already rolled the dice for the latest post ID
+					const userRolled = await context.redis.hGet(postUserRollsKey, userID);
+					context.ui.showToast(`Stored roll: ${userRolled}`);
+					const roll = userRolled ? parseInt(userRolled) : NaN;
+					if (userRolled && !Number.isNaN(roll)) {
+						context.ui.showToast(`You have already rolled ${roll} for this post.`);
+						return roll;
+					}
+				} else {
+					return null;
+				}
+
+				// User has not rolled yet
+
+				// Simulate dice roll
+				const diceCount = 20;
+				const diceRoll = Math.floor(Math.random() * diceCount) + 1;
+
+				// Store the user's dice roll
+				const userRollIncr: StringDictionary = {};
+				userRollIncr[userID] = diceRoll.toString()
+
+				await context.redis.hSet(postUserRollsKey, userRollIncr);
+				context.ui.showToast(`Setting ${userRollIncr} for ${postUserRollsKey}`);
+				await context.redis.expire(postUserRollsKey, ExpireTime.day);
+
+				// Save back to Redis
+				await context.redis.hIncrBy(totalKey, diceRoll.toString(), 1);
+				await context.redis.expire(totalKey, ExpireTime.day);
+
+				return diceRoll;
+			} catch (e) {
+				console.error('Error during dice roll:', e);
+				context.ui.showToast(`${e}`);
+				throw new Error('dice-error');
+			}
+		}, { depends: [gameData?.postID] }
+	);
 
 	// Fetch dice roll totals from Redis
 	const {
-		data: rollTotalsString,
+		data: topRolls,
 		loading: loadingRollTotals,
 		error: rollTotalsError,
 	} = useAsync(
 		async () => {
-			if (!gameData || !gameData.postID) return '{}';
-			const totalKey = `diceRollTotals:${gameData.postID}`;
-			const totalsString = (await context.redis.get(totalKey)) ?? '{}';
-			return totalsString;
-		},
-		{ depends: [gameData?.postID] }
+			if (!gameData || !gameData.postID) return null;
+			const rollTotals = (await context.redis.hGetAll(totalKey)) ?? {};
+
+			// Find the top roll
+			let topRoll = '';
+			let topCount = -Infinity;
+
+			for (const [roll, countString] of Object.entries(rollTotals)) {
+				const count = parseInt(countString);
+				if (typeof count === 'number' && !Number.isNaN(count) && count > topCount) {
+					topRoll = roll;
+					topCount = count;
+				}
+			}
+
+			// Handle the case where no rolls are found
+			if (topRoll === '') return null;
+
+			return { topRoll: topRoll, topCount: topCount };
+
+		}, { depends: [gameData?.postID, userRoll] }
 	);
 
-	const rollTotals = JSON.parse(rollTotalsString ?? '{}');
-
-	// Handle dice icon click
-	const handleDiceClick = async () => {
-		if (!gameData || !gameData.postID) return;
-
-		try {
-			// Get user ID
-			const user = await context.reddit.getCurrentUser();
-			let hasRolled = false;
-			const userID = user?.id ?? "anonymous";
-			const userKey = `diceRoll:${gameData.postID}:${userID}`;
-			if (user) {
-				// Check if the user has already rolled the dice for the latest post ID
-				const userRolled = await context.redis.get(userKey);
-				hasRolled = userRolled ? true : false;
-			} else {
-				hasRolled = true;
-			}
-
-			if (hasRolled) {
-				// User has already rolled
-				context.ui.showToast('You have already rolled the dice for this post.');
-				return;
-			}
-
-			// User has not rolled yet
-
-			// Simulate dice roll
-			const diceCount = 20;
-			const diceRoll = Math.floor(Math.random() * diceCount) + 1;
-
-			setUserRoll(diceRoll);
-			// Store the user's dice roll
-			await context.redis.set(userKey, 'rolled');
-
-			// Store the dice roll in Redis
-			const totalKey = `diceRollTotals:${gameData.postID}`;
-			const currentTotalsString = (await context.redis.get(totalKey)) ?? '{}';
-			const currentTotals = JSON.parse(currentTotalsString);
-
-			// Increment the count for the rolled number
-			currentTotals[diceRoll] = (currentTotals[diceRoll] || 0) + 1;
-
-			// Save back to Redis
-			await context.redis.set(totalKey, JSON.stringify(currentTotals));
-
-			// Update roll totals
-			rollTotals[diceRoll] = currentTotals[diceRoll];
-		} catch (error) {
-			console.error('Error during dice roll:', error);
-			context.ui.showToast('An error occurred while rolling the dice.');
-		}
-	};
-
-	// Render the component
 	return (
 		<vstack alignment='center middle' height={100}>
 			<vstack backgroundColor='neutral-background' padding="medium" cornerRadius="medium" gap="medium" alignment="center">
-				{userRoll ? (
-					<>
-						{/* Display dice roll totals */}
-						{Object.keys(rollTotals).length > 0 && (
-							<vstack padding="medium" cornerRadius="medium" gap="medium" alignment="center">
-								<text style="heading" size="medium">
-									Dice Roll Totals
-								</text>
-								{Object.entries(rollTotals).map(([roll, count]) => (
-									<text key={roll}>{`Rolled ${roll}: ${count} times`}</text>
-								))}
-							</vstack>
-						)}
-					</>
-				) : (
+				<hstack alignment='center middle'>
+					<button
+						icon="close-fill"
+						onPress={() => { setShowDiceView(false) }}
+					/>
+					<spacer grow />
+					<text>
+						{!loadingUserRoll && !userRollError && userRoll ? `You rolled a ${userRoll}!` : "Rolling…"}
+					</text>
+					<spacer grow />
+				</hstack>
+				{!userRollError &&
 					<image
-						url="D20-loop-all.gif"
+						url={"D20-loop-all.gif"}
 						imageWidth={250}
 						imageHeight={250}
-						description="Generative artwork: Fuzzy Fingers"
+						description="Rolling Die"
 					/>
-				)}
-				<button onPress={() => { setShowDiceView(false) }}>Done</button>
+				}
+				{/* Display dice roll totals */}
+				{(userRollError || userRoll) && topRolls &&
+					<hstack>
+						<text>{`Today's Top Roll: ${topRolls.topRoll} (${topRolls.topCount} users)`}</text>
+					</hstack>
+				}
 			</vstack>
 		</vstack>
 	)
---
+++ src/utils/utils.ts
@@ -0,0 +1,12 @@
+import { RedisClient } from "@devvit/public-api";
+
+export interface StringDictionary {
+	[key: string]: string;
+}
+
+export enum ExpireTime {
+	day = 86400,
+	week = 604800,
+	month = 2.628e+6,
+	year = 3.154e+7,
+}
